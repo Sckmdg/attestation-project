b1eb237cea60161be2e746fdc1ce1e3446063066
aminov <rustam.aminov@sibers.com>
2017-11-19 12:47:31 +0600

    add editor component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img src="./assets/logo.png">
     <ToDoList :items="items"/>
+    <Editor />
   </div>
 </template>
 
 <script>
 import ToDoList from './components/ToDoList'
+import Editor from './components/Editor'
 
 export default {
   name: 'app',
   components: {
-    ToDoList
+    ToDoList,
+    Editor
   },
   data () {
     return {
@@ -27,12 +30,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="editor-container">
+    <div>
+      <label>Name:</label>
+      <input type="text">
+    </div>
+
+    <div>
+      <label>Status:</label>
+      <select>
+        <option>True</option>
+        <option>False</option>
+      </select>
+    </div>
+
+    <button>Add</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Editor',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .editor-container {
+    width: 260px;
+    border: 1px solid;
+    margin: 0 auto;
+    margin-top: 25px;
+    padding: 15px;
+  }
+
+  .editor-container div {
+    width: 100%;
+    float: left;
+    margin-bottom: 15px;
+  }
+
+  div label, div input, div select {
+    float: left;
+  }
+
+  div input, div select {
+    margin-left: 15px;
+  }
+</style>
