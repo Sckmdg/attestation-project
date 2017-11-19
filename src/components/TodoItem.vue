090e4e4bb99ed7e3dd02bc6cafa905255608669c
aminov <rustam.aminov@sibers.com>
2017-11-19 20:27:54 +0600

    add fade effect

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,22 +1,24 @@
 <template>
-  <li>
-    <div>{{ item.name }}</div>
-    <div>
-      <input
-        type="checkbox"
-        value="item.name"
-        id="item.name"
-        v-model="item.status"
-        :checked="item.status"
-      />
-      <label for="item.name">Learned</label>
-    </div>
-    <div>
-    <button @click="deleteItem(item.id)">
-        Delete
-      </button>
-    </div>
-  </li>
+  <transition name="fade">
+    <li>
+      <div>{{ item.name }}</div>
+      <div>
+        <input
+          type="checkbox"
+          value="item.name"
+          id="item.name"
+          v-model="item.status"
+          :checked="item.status"
+        />
+        <label for="item.name">Learned</label>
+      </div>
+      <div>
+      <button @click="deleteItem(item.id)">
+          Delete
+        </button>
+      </div>
+    </li>
+  </transition>
 </template>
 
 <script>
@@ -54,5 +56,13 @@
     width: 33%;
     height: 25px;
     text-align: center;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0
   }
 </style>
